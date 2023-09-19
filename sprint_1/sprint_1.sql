3c609fc4832c1980328f376c09fd472145e52f5c
erups <e.rupshis@gmail.com>
2023-09-19 16:06:52 +0200

    fix according review

--- sprint_1/sprint_1.sql
+++ sprint_1/sprint_1.sql
@@ -21,7 +21,7 @@ CREATE TABLE raw_data.sales (
 	id INTEGER PRIMARY KEY,
 	auto VARCHAR(100) NOT NULL,
 	gasoline_consumption NUMERIC(3,1),
-	price NUMERIC NOT NULL,
+	price NUMERIC(9, 2) NOT NULL,
 	date DATE NOT NULL,
 	person_name VARCHAR(50) NOT NULL,
 	phone VARCHAR(50),
@@ -35,18 +35,6 @@ FROM 'C:\tmp\cars.csv' WITH CSV HEADER NULL 'null';
 CREATE SCHEMA IF NOT EXISTS car_shop;
 
 -- PARSING CARS.
-	-- CAR's BRANDS.
-	CREATE TABLE car_shop.brands (
-		id SMALLSERIAL PRIMARY KEY, -- 4 byte is not required due to total count of brands won't exceed 2^16.
-		name VARCHAR(20) UNIQUE NOT NULL -- Not expecting name bigger than 20 symbols.
-	);
-
-	INSERT INTO car_shop.brands(name)
-	SELECT DISTINCT
-		SPLIT_PART(auto, ' ', 1) AS brand_name
-	FROM raw_data.sales
-	GROUP BY brand_name;
-	
 	-- CAR's BRANDS ORIGIN.
 	CREATE TABLE car_shop.brands_origins (
 		id SMALLSERIAL PRIMARY KEY, -- countries count is below 210.
@@ -59,17 +47,37 @@ CREATE SCHEMA IF NOT EXISTS car_shop;
 	FROM raw_data.sales
 	GROUP BY brand_origin;
 	
+	-- CAR's BRANDS.
+	CREATE TABLE car_shop.brands (
+		id SMALLSERIAL PRIMARY KEY, -- 4 byte is not required due to total count of brands won't exceed 2^16.
+		name VARCHAR(20) UNIQUE NOT NULL, -- Not expecting name bigger than 20 symbols.
+		origin_id INT2 DEFAULT NULL -- countries count is below 210.
+	);
+
+	INSERT INTO car_shop.brands(name, origin_id)
+	SELECT
+		SPLIT_PART(auto, ' ', 1) AS brand_name,
+		car_shop.brands_origins.id AS origin_id
+	FROM raw_data.sales
+	LEFT JOIN car_shop.brands_origins ON brand_origin = car_shop.brands_origins.name
+	GROUP BY brand_name, origin_id;
+
 	-- CAR's MARKS.
 	CREATE TABLE car_shop.marks (
 		id SERIAL PRIMARY KEY, -- potentially can exceed 2^16.
-		name VARCHAR(25) UNIQUE NOT NULL -- longest one (Shàngqì Dàzhòng Pàsàtè Língdù) - chatgpt.
+		name VARCHAR(25) UNIQUE NOT NULL, -- longest one (Shàngqì Dàzhòng Pàsàtè Língdù) - chatgpt.
+		consumption NUMERIC(3,1) DEFAULT 0 CHECK (consumption >= 0) -- it's easier to keep in preformated type, but also float8 can be used. 
 	);
 
-	INSERT INTO car_shop.marks(name)
+	INSERT INTO car_shop.marks(name, consumption)
 	SELECT
-		SUBSTR(SPLIT_PART(auto, ', ', 1), STRPOS(auto, ' ') + 1)  AS mark_name
+		SUBSTR(SPLIT_PART(auto, ', ', 1), STRPOS(auto, ' ') + 1)  AS mark_name,
+		CASE 
+			WHEN gasoline_consumption IS NULL THEN 0
+			ELSE gasoline_consumption::NUMERIC(3,1)
+		END AS consumption
 	FROM raw_data.sales
-	GROUP BY mark_name;
+	GROUP BY mark_name, consumption;
 	
 	-- CAR's COLORS.
 	CREATE TABLE car_shop.colors (
@@ -83,21 +91,24 @@ CREATE SCHEMA IF NOT EXISTS car_shop;
 	FROM raw_data.sales
 	GROUP BY color_name;
 	
-	-- CAR's FUEL CONSUMPTION.
-	CREATE TABLE car_shop.fuel_consumptions (
-		id SMALLSERIAL PRIMARY KEY, -- consumptions delta step is 0.1. Total values count doesn't exceeds 1000.
-		value NUMERIC(3,1) DEFAULT 0 CHECK (value >= 0) -- it's easier to keep in preformated type, but also float8 can be used. 
+	-- CAR's CONFIGS
+	CREATE TABLE car_shop.configs (
+		id SERIAL PRIMARY KEY,
+		brand_id INT2 NOT NULL,
+		mark_id INTEGER NOT NULL,
+		color_id INT2 NOT NULL
 	);
 	
-	INSERT INTO car_shop.fuel_consumptions(value)
+	INSERT INTO car_shop.configs (brand_id, mark_id, color_id)
 	SELECT
-		CASE 
-			WHEN gasoline_consumption IS NULL THEN 0
-			ELSE gasoline_consumption::NUMERIC(3,1)
-		END AS value
+		car_shop.brands.id,
+		car_shop.marks.id,
+		car_shop.colors.id
 	FROM raw_data.sales
-	GROUP BY value;
-	
+	LEFT JOIN car_shop.brands ON SPLIT_PART(auto, ' ', 1) = car_shop.brands.name
+	LEFT JOIN car_shop.marks ON SUBSTR(SPLIT_PART(auto, ', ', 1), STRPOS(auto, ' ') + 1) = car_shop.marks.name
+	LEFT JOIN car_shop.colors ON SPLIT_PART(auto, ' ',-1) = car_shop.colors.name
+	GROUP BY (car_shop.brands.id, car_shop.marks.id, car_shop.colors.id);
 
 -- PARSING CLIENTS.
 	-- CLIENT's NAMES AND PHONE.
@@ -119,37 +130,30 @@ CREATE SCHEMA IF NOT EXISTS car_shop;
 -- SALES TABLE GENERATION.
 CREATE TABLE car_shop.sales (
 	id SERIAL PRIMARY KEY,
-	brand_id INT2 REFERENCES car_shop.brands(id),
-	mark_id INT2 REFERENCES car_shop.marks(id),
-	color_id INT2 REFERENCES car_shop.colors(id),
-	gasoline_consumption_id INT2 REFERENCES car_shop.fuel_consumptions(id),
+	config_id INTEGER REFERENCES car_shop.configs(id),
 	price NUMERIC NOT NULL,
 	date DATE NOT NULL,
 	client_id INTEGER REFERENCES car_shop.clients(id),
-	discount INT2 DEFAULT 0,
-	brand_origin_id INT2 REFERENCES car_shop.brands_origins(id)
+	discount INT2 DEFAULT 0
 );
 
-INSERT INTO car_shop.sales(brand_id, mark_id, color_id, gasoline_consumption_id, price, date, client_id, discount, brand_origin_id)
+INSERT INTO car_shop.sales(config_id, price, date, client_id, discount)
 SELECT
-	car_shop.brands.id,
-	car_shop.marks.id,
-	car_shop.colors.id,
-	car_shop.fuel_consumptions.id,
+	car_shop.configs.id,
 	price,
 	date,
 	car_shop.clients.id,
-	discount,
-	car_shop.brands_origins.id
+	discount
 FROM raw_data.sales
 LEFT JOIN car_shop.brands ON SPLIT_PART(auto, ' ', 1) = car_shop.brands.name
 LEFT JOIN car_shop.marks ON SUBSTR(SPLIT_PART(auto, ', ', 1), STRPOS(auto, ' ') + 1) = car_shop.marks.name
 LEFT JOIN car_shop.colors ON SPLIT_PART(auto, ' ', -1) = car_shop.colors.name
-LEFT JOIN car_shop.fuel_consumptions ON gasoline_consumption = car_shop.fuel_consumptions.value
+LEFT JOIN car_shop.configs ON (car_shop.brands.id = car_shop.configs.brand_id AND 
+								   car_shop.marks.id = car_shop.configs.mark_id 
+								   AND car_shop.colors.id = car_shop.configs.color_id)
 LEFT JOIN car_shop.clients ON (SPLIT_PART(person_name, ' ', 1) = car_shop.clients.first_name AND
 						  SPLIT_PART(person_name, ' ', 2) = car_shop.clients.last_name AND
-						  raw_data.sales.phone = car_shop.clients.phone)
-LEFT JOIN car_shop.brands_origins ON brand_origin = car_shop.brands_origins.name;
+						  raw_data.sales.phone = car_shop.clients.phone);
 
 SELECT * FROM car_shop.sales;
 
@@ -161,9 +165,10 @@ FROM raw_data.sales;
 SELECT
 	car_shop.brands.name AS brand_name,
 	EXTRACT(YEAR FROM date)::INT2 AS year,
-	AVG(price)::NUMERIC(9,2) AS price_avg
+	ROUND(AVG(price), 2) AS price_avg
 FROM car_shop.sales AS sales
-LEFT JOIN car_shop.brands ON car_shop.brands.id = sales.brand_id
+LEFT JOIN car_shop.configs ON car_shop.configs.id = sales.config_id
+LEFT JOIN car_shop.brands ON car_shop.brands.id = car_shop.configs.brand_id
 GROUP BY brand_name, year
 ORDER BY brand_name, year;
 
@@ -171,7 +176,7 @@ ORDER BY brand_name, year;
 SELECT
 	EXTRACT(MONTH FROM date)::INT2 AS month,
 	EXTRACT(YEAR FROM date)::INT2 AS year,
-	AVG(price)::NUMERIC(9,2) AS price_avg
+	ROUND(AVG(price), 2)::NUMERIC(9,2) AS price_avg
 FROM car_shop.sales AS s
 WHERE 
 	EXTRACT(YEAR FROM s.date) = 2022
@@ -183,8 +188,9 @@ SELECT
 	CONCAT_WS(' ', car_shop.clients.first_name, car_shop.clients.last_name) AS person,
 	STRING_AGG(CONCAT_WS(' ', car_shop.brands.name, car_shop.marks.name), ', ') AS cars
 FROM car_shop.sales
-LEFT JOIN car_shop.brands ON car_shop.brands.id = sales.brand_id
-LEFT JOIN car_shop.marks ON car_shop.marks.id = sales.mark_id
+LEFT JOIN car_shop.configs ON car_shop.configs.id = sales.config_id
+LEFT JOIN car_shop.brands ON car_shop.brands.id = car_shop.configs.brand_id
+LEFT JOIN car_shop.marks ON car_shop.marks.id = car_shop.configs.mark_id
 LEFT JOIN car_shop.clients ON car_shop.clients.id = sales.client_id
 GROUP BY person
 ORDER BY person;
@@ -192,10 +198,13 @@ ORDER BY person;
 --STAGE 2. TASK 5.
 SELECT
 	car_shop.brands_origins.name AS brand_origin,
-	MAX(100 * price / (100 - discount))::NUMERIC(9, 2) AS price_max,
-	MIN(100 * price / (100 - discount))::NUMERIC(9, 2) AS price_min
-FROM car_shop.sales
-INNER JOIN car_shop.brands_origins ON car_shop.sales.brand_origin_id = car_shop.brands_origins.id
+	ROUND(MAX(100 * price / (100 - discount)), 2) AS price_max,
+	ROUND(MIN(100 * price / (100 - discount)), 2) AS price_min
+FROM car_shop.sales AS sales
+LEFT JOIN car_shop.configs ON car_shop.configs.id = sales.config_id
+LEFT JOIN car_shop.brands ON car_shop.brands.id = car_shop.configs.brand_id
+INNER JOIN car_shop.brands_origins ON car_shop.brands_origins.id = car_shop.brands.origin_id
+WHERE car_shop.brands_origins.name IS NOT NULL
 GROUP BY brand_origin;
 
 --STAGE 2. TASK 6.
@@ -203,5 +212,4 @@ SELECT
 	COUNT(*) AS persons_from_usa_count
 FROM car_shop.clients AS c
 WHERE c.phone LIKE '+1%';
-
 	
